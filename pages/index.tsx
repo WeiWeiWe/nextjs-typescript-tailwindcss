a25d07b74e2d22d082059713fd6d42e31b25644f
Wei <aszx70293@gmail.com>
2023-07-25 15:39:19 +0800

    create a new project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline text-gray-400">Hello world!</h1>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
