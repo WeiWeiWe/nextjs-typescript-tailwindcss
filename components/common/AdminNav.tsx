bc0a5081b8e18c325d5ca7593f6cf0e6a0e48493
Wei <aszx70293@gmail.com>
2023-07-27 21:24:19 +0800

    20230727 - 1

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,20 +1,37 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import { IconType } from 'react-icons';
 import Logo from '../common/Logo';
 
-interface IProps {}
+interface IProps {
+  navItems: { label: string; icon: IconType; href: string }[];
+}
 
-const AdminNav: FC<IProps> = () => {
+const AdminNav: FC<IProps> = ({ navItems }) => {
   return (
     <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark">
       <Link href="/admin">
-        <a className="flex items-center space-x-2 p-3">
+        <a className="flex items-center space-x-2 p-3 mb-10">
           <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
-          <span className="fill-highlight-light dark:fill-highlight-dark text-xl font-semibold">
+          <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
             Admin
           </span>
         </a>
       </Link>
+      <div className="space-y-6">
+        {Array.isArray(navItems) &&
+          navItems?.length > 0 &&
+          navItems.map((item) => {
+            return (
+              <Link key={item?.href} href={item?.href || '/admin'}>
+                <a className="flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition">
+                  {item?.icon && <item.icon size={24} />}
+                  <span className="ml-2">{item?.label || ''}</span>
+                </a>
+              </Link>
+            );
+          })}
+      </div>
     </nav>
   );
 };
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,12 +1,31 @@
 import { NextPage } from 'next';
 import AdminNav from '../../components/common/AdminNav';
+import {
+  AiOutlineDashboard,
+  AiOutlineContainer,
+  AiOutlineTeam,
+  AiOutlineMail,
+  AiOutlineContacts,
+} from 'react-icons/ai';
 
 interface IProps {}
+
+const navItems = [
+  { href: '/admin', icon: AiOutlineDashboard, label: 'Dashboard' },
+  { href: '/admin/posts', icon: AiOutlineContainer, label: 'Posts' },
+  { href: '/admin/users', icon: AiOutlineTeam, label: 'Users' },
+  { href: '/admin/comments', icon: AiOutlineMail, label: 'Comments' },
+  { href: '/admin/contact', icon: AiOutlineContacts, label: 'Contact' },
+];
 
 const Admin: NextPage<IProps> = () => {
   return (
     <div>
-      <AdminNav />
+      <AdminNav
+        {...{
+          navItems,
+        }}
+      />
     </div>
   );
 };
