c26d45d65fda6346c1e01d39548c086ea3b2f17f
Wei <aszx70293@gmail.com>
2023-08-02 23:23:21 +0800

    20230802 - 1

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -1,23 +1,52 @@
-import { FC } from 'react';
+import { useState, useRef, FC } from 'react';
 import Link from 'next/link';
 import { IconType } from 'react-icons';
-import { RiMenuFoldFill } from 'react-icons/ri';
+import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri';
 import Logo from '../common/Logo';
 
 interface IProps {
   navItems: { label: string; icon: IconType; href: string }[];
 }
 
+const NAV_OPEN_WIDTH = 'w-60';
+const NAV_CLOSE_WIDTH = 'w-12';
+
 const AdminNav: FC<IProps> = ({ navItems }) => {
+  const navRef = useRef<HTMLElement>(null);
+  const [visible, setVisible] = useState(true);
+
+  const handleOpenOrCloseNav = () => {
+    const { current } = navRef;
+
+    if (!current) {
+      return;
+    }
+
+    if (visible) {
+      current.classList.remove(NAV_OPEN_WIDTH);
+      current.classList.add(NAV_CLOSE_WIDTH);
+    } else {
+      current.classList.remove(NAV_CLOSE_WIDTH);
+      current.classList.add(NAV_OPEN_WIDTH);
+    }
+
+    setVisible(!visible);
+  };
+
   return (
-    <nav className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between">
+    <nav
+      ref={navRef}
+      className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between"
+    >
       <div>
         <Link href="/admin">
           <a className="flex items-center space-x-2 p-3 mb-10">
             <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
-            <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
-              Admin
-            </span>
+            {visible && (
+              <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
+                Admin
+              </span>
+            )}
           </a>
         </Link>
         <div className="space-y-6">
@@ -28,15 +57,24 @@ const AdminNav: FC<IProps> = ({ navItems }) => {
                 <Link key={item?.href} href={item?.href || '/admin'}>
                   <a className="flex items-center text-highlight-light dark:text-highlight-dark text-xl p-3 hover:scale-[0.98] transition">
                     {item?.icon && <item.icon size={24} />}
-                    <span className="ml-2">{item?.label || ''}</span>
+                    {visible && (
+                      <span className="ml-2">{item?.label || ''}</span>
+                    )}
                   </a>
                 </Link>
               );
             })}
         </div>
       </div>
-      <button className="text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end">
-        <RiMenuFoldFill size={25} />
+      <button
+        onClick={handleOpenOrCloseNav}
+        className="text-highlight-light dark:text-highlight-dark p-3 hover:scale-[0.98] transition self-end"
+      >
+        {visible ? (
+          <RiMenuFoldFill size={25} />
+        ) : (
+          <RiMenuUnfoldFill size={25} />
+        )}
       </button>
     </nav>
   );
