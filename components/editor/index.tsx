3ab9d938f72b49afd9659299c7f8730bb32e9ebc
Wei <aszx70293@gmail.com>
2023-08-25 22:41:28 +0800

    20230825 - 1

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -2,18 +2,30 @@ import { FC } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Underline from '@tiptap/extension-underline';
+import Placeholder from '@tiptap/extension-placeholder';
 import ToolBar from './ToolBar';
 
 interface IProps {}
 
 const Editor: FC<IProps> = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Underline],
+    extensions: [
+      StarterKit,
+      Underline,
+      Placeholder.configure({ placeholder: 'Type something' }),
+    ],
+    editorProps: {
+      attributes: {
+        class:
+          'prose prose-lg focus:outline-none dark:prose-invert max-w-full mx-auto h-full',
+      },
+    },
   });
 
   return (
     <div className="p-3 dark:bg-primary-dark bg-primary transition">
       <ToolBar editor={editor} />
+      <div className="h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3"></div>
       <EditorContent editor={editor} />
     </div>
   );
