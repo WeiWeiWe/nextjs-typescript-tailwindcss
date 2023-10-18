74969ed9dfa20a8f9c1c80cb425b5ac8e905bd6e
Wei <aszx70293@gmail.com>
2023-10-18 21:12:31 +0800

    20231018 - 3

---
+++ components/common/ConfirmModal.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import classnames from 'classnames';
+import { ImSpinner3 } from 'react-icons/im';
+import ModelContainer, { ModelContainerIProps } from './ModelContainer';
+
+interface IProps extends ModelContainerIProps {
+  title: string;
+  subTitle: string;
+  busy?: boolean;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+}
+
+const commonBtnClasses = 'px-3 py-1 text-white rounded';
+
+const ConfirmModal: FC<IProps> = ({
+  visible,
+  title,
+  subTitle,
+  busy = false,
+  onClose,
+  onCancel,
+  onConfirm,
+}) => {
+  return (
+    <ModelContainer visible={visible} onClose={onClose}>
+      <div className="bg-primary-dark dark:bg-primary rounded p-3">
+        {/* title */}
+        <p className="text-primary dark:text-primary-dark font-semibold text-lg">
+          {title || ''}
+        </p>
+        {/* sub title */}
+        <p className="text-primary dark:text-primary-dark">{subTitle || ''}</p>
+        {/* buttons */}
+        {busy && (
+          <p className="flex items-center space-x-2 text-primary dark:text-primary-dark pt-2">
+            <ImSpinner3 className="animate-spin" />
+            <span>Please wait</span>
+          </p>
+        )}
+        {!busy && (
+          <div className="flex items-center space-x-2 pt-2">
+            <button className={classnames(commonBtnClasses, 'bg-red-500')}>
+              Confirm
+            </button>
+            <button className={classnames(commonBtnClasses, 'bg-blue-500')}>
+              Cancel
+            </button>
+          </div>
+        )}
+      </div>
+    </ModelContainer>
+  );
+};
+
+export default ConfirmModal;
