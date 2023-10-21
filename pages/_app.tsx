e54a7c1893f326354b10010e96f2076455a7dcca
Wei <aszx70293@gmail.com>
2023-10-21 14:54:10 +0800

    20231021 - 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import nProgress from 'nprogress';
 
 interface IProps {
   session?: Session | null;
 }
+
+nProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps<IProps>) {
   return (
