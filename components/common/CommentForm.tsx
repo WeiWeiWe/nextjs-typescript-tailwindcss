eab56e9237df23de033378c3dc3c9b2c653d0e37
Wei <aszx70293@gmail.com>
2023-10-25 15:44:08 +0800

    20231025 - 1 - Comment form ui done.

--- components/common/CommentForm.tsx
+++ components/common/CommentForm.tsx
@@ -1,9 +1,33 @@
 import { FC } from 'react';
+import { EditorContent } from '@tiptap/react';
+import useEditorConfig from '@/hooks/useEditorConfig';
+import ActionButton from './ActionButton';
 
-interface IProps {}
+interface IProps {
+  title?: string;
+}
 
-const CommentForm: FC<IProps> = () => {
-  return <div>CommentForm</div>;
+const CommentForm: FC<IProps> = ({ title }) => {
+  const { editor } = useEditorConfig({ placeholder: 'Add your comment...' });
+
+  return (
+    <div>
+      {title ? (
+        <h1 className="text-xl text-primary-dark dark:text-primary font-semibold py-3">
+          {title}
+        </h1>
+      ) : null}
+      <EditorContent
+        className="min-h-[200px] border-2 border-secondary-dark rounded p-2"
+        editor={editor}
+      />
+      <div className="flex justify-end py-3">
+        <div className="inline-block">
+          <ActionButton title="Submit" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentForm;
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -46,7 +46,7 @@ const SinglePost: NextPage<Props> = ({ post }) => {
         </div>
         <div className="py-20">
           {userProfile ? (
-            <CommentForm />
+            <CommentForm title="Add comment" />
           ) : (
             <div className="flex flex-col items-end space-y-2">
               <h3 className="text-secondary-dark text-xl font-semibold">
