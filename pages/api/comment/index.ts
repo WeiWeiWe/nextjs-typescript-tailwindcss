828e9c8bf928879af91a2cf57e4c7d1bca8c1286
Wei <aszx70293@gmail.com>
2023-10-29 21:01:28 +0800

    20231029 - 1 - remove comment api done.

--- pages/api/comment/index.ts
+++ pages/api/comment/index.ts
@@ -1,4 +1,5 @@
 import { NextApiHandler } from 'next';
+import { isValidObjectId } from 'mongoose';
 import { isAuth } from '@/lib/utils';
 import { validateSchema, commentValidationSchema } from '@/lib/validator';
 import dbConnect from '@/lib/dbConnect';
@@ -11,7 +12,8 @@ const handler: NextApiHandler = (req, res) => {
   switch (method) {
     case 'POST':
       return createNewComment(req, res);
-
+    case 'DELETE':
+      return removeComment(req, res);
     default:
       res.status(404).send('Not found!');
   }
@@ -41,6 +43,36 @@ const createNewComment: NextApiHandler = async (req, res) => {
 
   await comment.save();
   res.status(201).json(comment);
+};
+
+const removeComment: NextApiHandler = async (req, res) => {
+  const user = await isAuth(req, res);
+  if (!user) return res.status(403).json({ error: 'unauthorized request!' });
+
+  const { commentId } = req.query;
+  if (!commentId || !isValidObjectId(commentId))
+    return res.status(422).json({ error: 'Invalid request!' });
+
+  const comment = await Comment.findOne({ _id: commentId, owner: user?.id });
+  if (!comment) return res.status(404).json({ error: 'Comment not found!' });
+
+  // if chief comment remove other related comments (replies) as well
+  if (comment?.chiefComment) {
+    await Comment.deleteMany({ repliedTo: commentId });
+  } else {
+    // if this is the reply comment, remove from the chief comments replies section.
+    const chiefComment = await Comment.findById(comment.repliedTo);
+    if (chiefComment?.replies?.includes(commentId as any)) {
+      chiefComment.replies = chiefComment.replies.filter(
+        (cId) => cId.toString() !== commentId
+      );
+      await chiefComment.save();
+    }
+  }
+
+  // then remove the actual comment
+  await Comment.findByIdAndDelete(commentId);
+  res.json({ removed: true });
 };
 
 export default handler;
